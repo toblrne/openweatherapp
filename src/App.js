b6e4815a59fef4a8222596108e205514374d8890
Ryan Gao <ryantchgao@googlemail.com>
2022-06-16 01:27:12 -0700

    added functionality for location input

--- src/App.js
+++ src/App.js
@@ -1,3 +1,37 @@
+import React, { useState } from 'react'
+import axios from 'axios'
 
+export default function App() {
+
+  const [data, setData] = useState({})
+
+  const [location, setLocation] = useState("")
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=895284fb2d2c50a520ea537456963d9c`
+
+  function getWeatherData() {
+    axios.get(url).then((response) => {
+      setData(response.data)
+      console.log(response.data)
+    })
+    setLocation("")
+  }
+
+  function handleChange(event) {
+    setLocation(event.target.value)
+  }
+
+  return (
+    <div>
+      <input
+        value={location}
+        type="text"
+        onChange={handleChange} />
+      <button onClick={getWeatherData}> get weather data</button>
+      {data.main ? <h1>{data.main.temp}</h1> : null}
+    </div>
+
+  )
+}
 
 
